d1f174a839ccf3efbdba35d3733c6df99554414f
SilasFr <silas23.fr@gmail.com>
2022-06-22 12:53:28 -0300

    feat: create dashboard layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Dashboard from './layouts/Dashboard';
 import Calculator from './pages';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Calculator />} />
+        <Route path="/" element={<Dashboard />}>
+          <Route path="/home" element={<Calculator />} />
+        </Route>
       </Routes>
     </BrowserRouter>
   );
---
+++ src/layouts/Dashboard.jsx
@@ -0,0 +1,24 @@
+import Container from '../components/Page';
+import styled from 'styled-components';
+
+export default function Dashboard({ children }) {
+  return (
+    <Container>
+      <StyledTitle>Salary Calculator</StyledTitle>
+      <DashboardComponent>{children}</DashboardComponent>
+    </Container>
+  );
+}
+
+const DashboardComponent = styled.div`
+  background: #fff;
+  margin: auto;
+  min-height: 600px;
+  border-radius: 5%;
+`;
+
+const StyledTitle = styled.h1`
+  color: #fff;
+  text-align: center;
+  margin: 30px 0 50px 0;
+`;
