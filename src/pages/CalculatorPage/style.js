8f20abf07c787f04a4bea04e4f3c5e8c5a3966fc
starunz <busouza01@gmail.com>
2022-06-23 23:35:18 -0500

    chore: the form is now a separate component and is being rendered in calculatorPage, created a utils folder to allocate the array of salary types

---
+++ src/pages/CalculatorPage/style.js
@@ -0,0 +1,25 @@
+import styled from 'styled-components';
+
+const Container = styled.div`
+	width: 70%;
+	height: 600px;
+
+	border-radius: 10px;
+
+	display: flex;
+	justify-content: center;
+	//flex-direction: column;
+	align-items: center;
+
+	background-color: #121212;
+	box-shadow: 0 3px 15px rgba(245, 40, 145, 0.8);
+
+	@media (max-width: 840px) {
+		width: 80%;
+		height: 100%;
+	}
+`;
+
+export {
+	Container,
+}
---
+++ src/utils/index.jsx
@@ -0,0 +1,44 @@
+const typeSalary = [
+	{
+		modality: 'CLT',
+		title: 'Salário bruto',
+		titleInput: 'Benefícios',
+		placeholder: 4000,
+		benefits: [
+			{
+				name: 'Vale Refeição/Alimentação',
+				value: 500
+			},
+			{
+				name: 'Plano de Saúde',
+				value: 800
+			},
+			{
+				name: 'Outros Benefícios',
+				value: 900
+			},
+		]
+	},
+	{
+		modality: 'PJ',
+		title: 'Salário bruto',
+		titleInput: 'Benefícios',
+		placeholder: 5000,
+		benefits: [
+			{
+				name: 'Vale Refeição/Alimentação',
+				value: 600
+			},
+			{
+				name: 'Plano de Saúde',
+				value: 700
+			},
+			{
+				name: 'Outros Benefícios',
+				value: 800
+			},
+		]
+	}
+]
+
+export default typeSalary;
