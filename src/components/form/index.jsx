843e60acb064a8d03337d1b4d222568a9bb71170
starunz <busouza01@gmail.com>
2022-06-24 01:50:38 -0500

    chore: route to summary added and bugs fixed

--- src/components/form/index.jsx
+++ src/components/form/index.jsx
@@ -1,62 +1,67 @@
-import { 
-	Calculate, 
-	FormInfos, 
-	ContainerInputsInfo, 
-	ContainerInfos, 
-	InputForm, 
-	Modality, 
-	Name, 
-	Title 
-} from "./style"
+import { useNavigate } from 'react-router-dom';
 
-import typeSalary from "../../utils";
+import {
+  Calculate,
+  FormInfos,
+  ContainerInputsInfo,
+  ContainerInfos,
+  InputForm,
+  Modality,
+  Name,
+  Title
+} from './style';
+
+import typeSalary from '../../utils';
 
 const Form = () => {
+  const navigate = useNavigate();
 
-	const calculate = (e) => {
-		e.preventDefault();
-		console.log('ue')
-	}
+  const calculate = (e) => {
+    e.preventDefault();
+    console.log('ue');
 
-	const handleChange = (e) => {
-		console.log({ [e.target.name]: e.target.value });
-	}
+    navigate('/summary-page');
+  };
 
-	return(
-		<FormInfos onSubmit={calculate}>
-			<ContainerInfos>
-				{
-					typeSalary.map(( typeSalary, index ) => (
-						<ContainerInputsInfo>
-							<Modality>{typeSalary.modality}</Modality>
+  const handleChange = (e) => {
+    console.log({ [e.target.name]: e.target.value });
+  };
 
-							<Title>{typeSalary.title}:</Title>
-							<InputForm key={index}
-								name={`${typeSalary.title}`}
-								placeholder={`R$ ${typeSalary.placeholder}`}
-								onChange={handleChange}
-							/>
+  return (
+    <FormInfos onSubmit={calculate}>
+      <ContainerInfos>
+        {
+          typeSalary.map((typeSalary, index) => (
+            <ContainerInputsInfo key={index}>
+              <Modality>{typeSalary.modality}</Modality>
 
-							<Title>{typeSalary.titleInput}:</Title>
-							{
-								typeSalary.benefits.map((benefits, index) => (
-									<>
-										<Name>{benefits.name}:</Name>
-										<InputForm key={index}
-											name={`${benefits.name}`}
-											placeholder={`R$ ${benefits.value}`}
-											onChange={handleChange}
-										/>
-									</>
-								))
-							}
-						</ContainerInputsInfo>
-					))
-				}
-			</ContainerInfos>
-			<Calculate type='submit'>Calcular</Calculate>
-		</FormInfos>
-	);
-}
+              <Title>{typeSalary.title}:</Title>
+              <InputForm key={index}
+                name={`${typeSalary.title}`}
+                placeholder={`R$ ${typeSalary.placeholder}`}
+                onChange={handleChange}
+              />
+
+              <Title>{typeSalary.titleInput}:</Title>
+              {
+                typeSalary.benefits.map((benefits, index) => (
+                  <>
+                    <Name>{benefits.name}:</Name>
+                    <InputForm key={index}
+                      name={`${benefits.name}`}
+                      placeholder={`R$ ${benefits.value}`}
+                      onChange={handleChange}
+                    />
+                  </>
+                ))
+              }
+            </ContainerInputsInfo>
+          ))
+        }
+      </ContainerInfos>
+      <Calculate type='submit'>Calcular</Calculate>
+    </FormInfos>
+  );
+};
 
 export default Form;
