5f8b0b9a46829c299207e47a58b07be74d719c88
starunz <busouza01@gmail.com>
2022-06-27 05:02:48 -0500

    chore: added assorted sizes

--- src/components/pageContainer/style.js
+++ src/components/pageContainer/style.js
@@ -2,10 +2,27 @@ import styled from 'styled-components';
 
 const Container = styled.div`
   display: flex;
+  flex-direction: column;
   justify-content: center;
   align-items: center;
 
   margin: calc((100vh - 600px) / 2) auto;
+
+  @media (max-width: 840px) {
+    margin: calc((100vh - 650px) / 2) auto;
+  }
+
+   @media (max-width: 600px) {
+    margin: calc((100vh - 700px) / 2) auto;
+  }
+
+  @media (max-width: 400px) {
+    margin: calc((100vh - 650px) / 2) auto;
+  }
+
+  @media (min-width: 840px) {
+    margin: calc((100vh - 600px) / 2) auto;
+  } 
 `;
 
 export {
